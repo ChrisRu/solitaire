1cd4b938bb98a9f423f1d4eb6fef8a656d02b4c9
Christian Ruigrok <christian@ruigrok.info>
2016-12-05 20:54:05 +0100

    Initial commit

---
+++ app/scripts/script.js
@@ -0,0 +1,67 @@
+let Card = function (num, type) {
+  this.num = num
+  this.type = type
+  this.numName = numToName(this.num)
+  this.typeName = typeToName(this.type)
+  this.element = function () {
+    let el = document.createElement('div')
+    el.classList.add('card')
+    el.classList.add(this.typeName[2])
+    el.innerHTML = '<span>' + this.numName + this.typeName[1] + '</span><div class="type">' + this.typeName[1] + '</div><span>' + this.numName + this.typeName[1] + '</span>'
+    return el
+  }
+}
+
+function typeToName (type) {
+  switch (type) {
+    case 0:
+      return ['Hearts', '♥', 'red']
+    case 1:
+      return ['Diamonds', '♦', 'red']
+    case 2:
+      return ['Clubs', '♣', 'black']
+    case 3:
+      return ['Spades', '♠', 'black']
+    default:
+      throw new Error('Card type "' + type + '" does not exist')
+  }
+}
+
+function numToName (num) {
+  if (num >= 0 && num <= 13) {
+    switch (num) {
+      case 0:
+        return 'A'
+      case 11:
+        return 'B'
+      case 12:
+        return 'Q'
+      case 13:
+        return 'K'
+      default:
+        return num
+    }
+  } else {
+    throw new Error('Num "' + num + '" does not exist')
+  }
+}
+
+let cards = [new Card(4, 1), new Card(1, 2), new Card(5, 0), new Card(11, 3)]
+for (let card of cards) {
+  document.getElementsByClassName('stacks')[0].appendChild(card.element())
+}
+
+// const full = document.getElementsByClassName('full')[0]
+
+
+/*
+
+=============================================================================
+=============================================================================
+=============================================================================
+=============================================================================
+=============================================================================
+
+*/
+
+
---
+++ gulpfile.js
@@ -0,0 +1,47 @@
+var gulp = require('gulp'),
+	sass = require('gulp-sass'),
+	nano = require('gulp-cssnano'),
+	prefix = require('gulp-autoprefixer'),
+	babel = require('gulp-babel'),
+	browserSync = require('browser-sync');
+
+gulp.task('default', ['serve'], function () {
+	gulp.watch('app/*.html', ['html']).on('change', browserSync.reload);
+	gulp.watch("app/scripts/*.js", ['babel']).on('change', browserSync.reload);
+	gulp.watch('app/styles/**/*.scss', ['sass']);
+});
+
+gulp.task('html', function() {
+	return gulp.src('app/index.html')
+		.pipe(gulp.dest('dist/'));
+});
+
+gulp.task('babel', function() {
+	return gulp.src('app/scripts/script.js')
+        .pipe(babel({
+            presets: ['es2015']
+        }))
+        .pipe(gulp.dest('dist/scripts/'));
+});
+
+gulp.task('sass', function () {
+	return gulp.src('app/styles/master.scss')
+		.pipe(sass({
+			outputStyle: 'compressed'
+		}).on('error', sass.logError))
+		.pipe(prefix({
+			browsers: ['> 1%', 'IE 8'], 
+            cascade: false
+		}))
+		.pipe(nano())
+		.pipe(gulp.dest('dist/styles/'))
+		.pipe(browserSync.stream());
+});
+
+gulp.task('serve', function() {
+	browserSync.init({
+		server: {
+			baseDir: "dist"
+		}
+	});
+});
